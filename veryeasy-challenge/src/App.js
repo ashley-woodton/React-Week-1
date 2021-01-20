8214437f3523b10fc294900a95f2d6db60b34b6e
ashley-woodton <ashley.woodton@gmail.com>
2021-01-20 15:52:03 -0500

    first commit

---
+++ veryeasy-challenge/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>Ashley Woodton</h1>
+        <p>123-456-7890</p>
+        <p>07/18/1997</p>
+      </div>
+    );
+  }
+}
+
+export default App;
